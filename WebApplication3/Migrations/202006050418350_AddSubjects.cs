5525e2e1d9d541f859ae24b5261591c4629f30a5
arkonq@gmail.com <arkonq@gmail.com>
2020-06-05 10:25:59 +0600

    Add project files.

---
+++ WebApplication3/Migrations/202006050418350_AddSubjects.cs
@@ -0,0 +1,34 @@
+﻿namespace WebApplication3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSubjects : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Subjects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Room = c.Int(nullable: false),
+                        Title = c.String(),
+                        CourseName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Students", "Subject_Id", c => c.Int());
+            CreateIndex("dbo.Students", "Subject_Id");
+            AddForeignKey("dbo.Students", "Subject_Id", "dbo.Subjects", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "Subject_Id", "dbo.Subjects");
+            DropIndex("dbo.Students", new[] { "Subject_Id" });
+            DropColumn("dbo.Students", "Subject_Id");
+            DropTable("dbo.Subjects");
+        }
+    }
+}
---
+++ WebApplication3/Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+	// Добавить класс Subject с полями Room, Title, List<Student> CourseName (название курса)
+	public class Subject
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public int Room { get; set; }
+
+		public string Title { get; set; }
+
+		public List<Student> Students { get; set; }
+
+		public string CourseName { get; set; }
+	}
+}
